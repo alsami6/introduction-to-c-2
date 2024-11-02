c40b924baed196aa9bc1c3d4543bc71755c9a3e0
alsami6 <alsami1209@gmail.com>
2024-11-02 22:06:10 +0600

    -

--- practice/module-15/Pointer-&-Array.c
+++ practice/module-15/Pointer-&-Array.c
@@ -1,21 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    int arr[5] = {1, 2, 3, 4, 5};
+    int a = 5, b=6;
+    int *x = &a, *y = &b;
 
-    int *p = arr;
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 
-
-    printf("%d %d\n", &arr[0], p);
-
-    printf("%d\n", (p+1));
-
-    int a = 6;
-    int *x = &a;
-
-
-    printf("%d %d\n", x, x+1);
-
+    printf("%d %d", a, b);
 
     return 0;
 }
