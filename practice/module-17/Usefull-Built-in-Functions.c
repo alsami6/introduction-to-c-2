6ab3d61dc15ed62b6dfe0704348b3fe43a324969
alsami6 <alsami1209@gmail.com>
2024-11-05 22:28:43 +0600

    _

---
+++ practice/module-17/Usefull-Built-in-Functions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    
+    double a = 10.5;
+
+
+    printf("%lf\n", ceil(a));
+    printf("%lf\n", floor(a));
+    printf("%lf\n", round(a));
+
+    printf("%lf\n", sqrt(25));
+
+    printf("%lf\n", pow(3, 2));
+
+
+    return 0;
+}
